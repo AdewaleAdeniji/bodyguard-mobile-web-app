b0a8a89a426989f918f8efa486498cf1466d51af
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2025-01-18 13:06:43 +0100

    disable cors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    origin: "*",
+    cors: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+      'Content-Security-Policy': "frame-ancestors 'self' *",
+      'X-Content-Security-Policy': "frame-ancestors 'self' *",
+      'X-WebKit-CSP': "frame-ancestors 'self' *",
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
